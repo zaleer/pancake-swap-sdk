fd55e33d4785a134b44c532baca6bff90ab7b75e
zale <zale_8@126.com>
2022-01-13 12:02:33 +0800

    修改合约地址

--- src/constants.ts
+++ src/constants.ts
@@ -19,7 +19,7 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+export const FACTORY_ADDRESS = '0x12935a417c26a30b0fFC7fa3d9d075acB1437317'
 
 export const INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
 
